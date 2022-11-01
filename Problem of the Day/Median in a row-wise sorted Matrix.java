f2c3ef947f0feeca59cd2bec0f997f629c3d6b0c
Sachin Kumar <110603659+iamsacstar@users.noreply.github.com>
2022-11-01 12:12:53 +0530

    Update Median in a row-wise sorted Matrix.java

--- Problem of the Day/Median in a row-wise sorted Matrix.java
+++ Problem of the Day/Median in a row-wise sorted Matrix.java
@@ -8,46 +8,21 @@
 class Solution {
     int median(int matrix[][], int R, int C) {
         // code here
-        class Solution {
-
-    int median(int matrix[][], int R, int C) {
-
         int size = R*C;
-
         int[] arr = new int[size];
-
         int index = 0;
-
         int i=0,j=0;
-
         while(i<R && j<C)   
-
         {
-
-                
-
             arr[index++] = matrix[i][j++];
-
-            
-
             if(j==C && i!=R-1)
-
             {
-
                 j=0;
-
                 i++;
-
             }
-
         }
-
-        
-
         Arrays.sort(arr);
-
         int median = (size+1)/2;
-
         return arr[median-1];
-
     }
+}
